67ea04abb489491adff1610e85c55e93a59d5681
Will Foran <willforan@gmail.com>
2021-06-28 11:42:59 -0400

    🚀 20210601 prisma2 working python<->arrington dll load

--- test_arrington.py
+++ test_arrington.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python3
 from lncdtask.externalcom import Arrington
-from psychopy import core
+from psychopy import core, event
+import os
 
-print("connecting ...")
-tracker = Arrington()
+VPXDLL = "C:/Users/Clark/Desktop/VPx64-Client/VPX_InterApp_64.dll"
+try:
+    if not os.path.exists(VPXDLL):
+        raise Exception(f"VPXDLL doesn't exist! '{VPXDLL}'")
+    else:
+        print(f"using {VPXDLL}")
 
-print("opening new file")
-tracker.new("test_file")
+    print("connecting ...")
+    tracker = Arrington(vpxDll=VPXDLL)
 
-print("start recording")
-tracker.start()
+    print("opening new file")
+    tracker.new("test_file")
+
+    print("start recording")
+    tracker.start()
 
 
-print("send event and waiting 1 second...")
-tracker.event("test event")
-core.wait(1)
+    print("send event and waiting 1 second...")
+    tracker.event("test event")
+    core.wait(1)
 
-print("again and wait 1 second...")
-tracker.event("different event")
-core.wait(1)
+    print("again and wait 1 second...")
+    tracker.event("different event")
+    core.wait(1)
 
-print("done")
-tracker.stop()
+    print("done")
+    tracker.stop()
 
+except Exception as e:
+    print(f"{e}")
+
+#print("anykey to quit")
+event.waitKeys()
