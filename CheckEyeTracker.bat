f85290c39ebcf666c9d54933c191d792a9ea8eca
ncanda <ncanda@UPMC51.acct.upmchs.net>
2021-07-13 15:28:39 -0400

    bat file to run arrington socket check

---
+++ CheckEyeTracker.bat
@@ -0,0 +1,7 @@
+SET "PYPATH=C:\Program Files\PsychoPy\python.exe"
+SET "TASKDIR=C:\Users\ncanda\Desktop\lncdtask"
+cd %TASKDIR%
+"%PYPATH%" "%TASKDIR%\test_arrington.py"
+@echo off
+echo ALL DONE. push anykey to close
+pause
