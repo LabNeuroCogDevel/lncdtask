a116990fba66346aa4e8d4b7d433cf5690cff039
WillForan <willforan@gmail.com>
2022-08-26 09:47:26 -0400

    🐛 dollarreward ttl: split from 1st arg string; correctly set lookup

--- lncdtask/dollarreward.py
+++ lncdtask/dollarreward.py
@@ -26,21 +26,56 @@ def eppos2relpos(x, orig_width=800):
     return (x-half_width)/half_width
 
 
-def ttl(trial, event, rew, pos):
+def ttl_wrap(code):
+    """
+    might int input: start and stop codes from library
+    otherwise is a string of trial event rew pos
+    sent to ttl()
+    """
+    if type(code) == str:
+        einfo = code.split(' ')
+        code = ttl(*einfo)
+        print(f"code: {code} from {einfo}")
+    return code
+
+def ttl(trial, event=None, rew=None, pos=None):
     """
     input is same as what is given to flip_at
     passed from there into
     callOnFlip(mark_func, *kargs)
     """
     rew_look = {'neu': 100, 'rew': 200}
-    evt_look = { 'iti': 10, 'ring': 20,
-                'prep': 30,  'dot': 40}
-    pos_look = {  '7': 1, '214': 2,
-                '426': 3, '633': 4}
+    evt_look = { 'iti': 10, 'ring': 20, 'prep': 20,
+                'cue': 30,  'dot': 40}
+    # though we were using input pos
+    # but actually getting calc screen -1 to 1
+    #pos_look = {  '7': 1, '214': 2,
+    #            '426': 3, '633': 4}
+    #pos_look= pos_look.get(str(pos), 5)
+    if pos is None:
+        pos = 9
+    else:
+        pos = float(pos)
+
+    if pos < -.7: #-.97
+        pos_look = 1
+    elif pos < -.5: # -.66
+        pos_look = 2
+    elif pos < 0: # -.33
+        pos_look = 3
+    elif pos < .5: # .33
+        pos_look = 4
+    elif pos < .9: # .66
+        pos_look = 5
+    elif pos < 1: # .97
+        pos_look = 6
+    else:
+        pos_look = 0 # iti -- no side
+
 
     v = rew_look.get(rew, 0) +\
         evt_look.get(event, 50) + \
-        pos_look.get(str(pos), 5)
+        pos_look
 
     return(v)
             
@@ -363,7 +398,7 @@ if __name__ == "__main__":
         elif run_info.info['EyeTracking'] == 'EEG':
             from externalcom import ParallelPortEEG
             port = int(run_info.info['LPTport'])
-            eyetracker = ParallelPortEEG(port, ttl, verbose=True)
+            eyetracker = ParallelPortEEG(port, lookup_func=ttl_wrap, verbose=True)
         else:
             eyetracker = None
 
