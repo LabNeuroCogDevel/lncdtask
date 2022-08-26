e4a52a20f677db8bc73b15d9188ea9a7c7f0bf14
WillForan <willforan@gmail.com>
2022-08-26 09:53:13 -0400

    👽 dollarreward/eeg.bat: User is LocalAdmin instead of Luna

--- desktop_run_dollarreward_eeg.bat
+++ desktop_run_dollarreward_eeg.bat
@@ -1,5 +1,5 @@
 SET "PYPATH=C:\Program Files\PsychoPy\python.exe"
-SET "TASKDIR=C:\Users\Luna\Desktop\lncdtask"
+SET "TASKDIR=C:\Users\LocalAdmin\Desktop\lncdtask"
 cd %TASKDIR%
 "%PYPATH%" "%TASKDIR%\lncdtask\dollarreward.py" "timing/dollar_reward_eeg1.tsv" "timing/dollar_reward_eeg2.tsv" "timing/dollar_reward_eeg3.tsv" "timing/dollar_reward_eeg4.tsv"
 @echo off
