3dee3903750a32a97343037cc8a6d248e4cd2985
WillForan <willforan@gmail.com>
2022-08-29 13:04:31 -0400

    ✨ dollarreward/view.R: write mr_times.tsv

--- timing/view.R
+++ timing/view.R
@@ -1,12 +1,23 @@
 library(dplyr)
 library(ggplot2)
 library(cowplot)
-fits <- Sys.glob('out/3[0-9]*s/v*/stddevtests.tsv') %>%
+library(glue)
+list_rings <- function(f) {
+   f <- gsub('stddevtests.tsv','events.txt', f)
+   glue("grep -o '[^(]*ring[^)]*' {f} |paste -sd,") %>%
+      system(intern=T) }
+flist <- Sys.glob('out/3[0-9]*s/v*/stddevtests.tsv')
+fits <- flist %>%
    lapply(function(f)
+        tryCatch(
           read.table(f, sep="\t", header=T) %>%
-          mutate(tdur=gsub('.*/([0-9.]+)s/.*', '\\1', f))) %>%
+          mutate(tdur=gsub('.*/([0-9.]+)s/.*', '\\1', f),
+                 tlist=sapply(f,list_rings)),
+        error=function(f )NULL)) %>%
    bind_rows
+write.table(file='mr_times.tsv', fits, row.names=F,quote=F)
 theme_set(theme_cowplot())
+
 
 plot_grid(ncol=2,
  ggplot(fits %>% filter(prep_neu.rew_LC<1.5)) +
