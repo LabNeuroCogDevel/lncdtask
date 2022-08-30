dc45d2ffeb71379a0b96ca977f63f6abbc68d890
WillForan <willforan@gmail.com>
2022-08-30 09:47:11 -0400

    ✨ dollarreward mr bat: use new timing files
    also remove duplicate bad case non-CRLF/dos version of bat file

--- desktop_run_dollarreward_noTR.bat
+++ desktop_run_dollarreward_noTR.bat
@@ -3,7 +3,7 @@ SET "ET_HOST=10.48.88.120"
 SET "PYPATH=C:\Program Files\PsychoPy\python.exe"
 SET "TASKDIR=C:\Users\Luna\Desktop\lncdtask"
 cd %TASKDIR%
-"%PYPATH%" "%TASKDIR%\lncdtask\dollarreward.py" --where mr  --nruns 2 "timing/dollar_reward_noTR_12228.tsv" "timing\dollar_reward_noTR_23337.tsv"
+"%PYPATH%" "%TASKDIR%\lncdtask\dollarreward.py" --where mr  --nruns 2  "timing/dollarreward/dollar_reward_noTR_304.2_10871.tsv"  "timing/dollarreward/dollar_reward_noTR_304.2_4341.tsv" "timing/dollarreward/dollar_reward_noTR_304.2_23695.tsv"  "timing/dollarreward/dollar_reward_noTR_304.2_5183.tsv" "timing/dollarreward/dollar_reward_noTR_304.2_25418.tsv"  "timing/dollarreward/dollar_reward_noTR_304.2_7243.tsv"
 @echo off
 echo ALL DONE. push anykey to close
 pause
--- desktop_run_dollarreward_notR.bat
+++
@@ -1,9 +0,0 @@
-SET "VPXDLL=C:/Windows/System32/VPX_InterApp_64.dll"
-SET "ET_HOST=10.48.88.120"
-SET "PYPATH=C:\Program Files\PsychoPy\python.exe"
-SET "TASKDIR=C:\Users\Luna\Desktop\lncdtask"
-cd %TASKDIR%
-"%PYPATH%" "%TASKDIR%\lncdtask\dollarreward.py" "timing/dollar_reward_noTR_12228.tsv" "timing\dollar_reward_noTR_23337.tsv"
-@echo off
-echo ALL DONE. push anykey to close
-pause
