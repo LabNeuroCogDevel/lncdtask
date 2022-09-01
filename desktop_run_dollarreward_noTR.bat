dfc97363c0f83f83ced43bd97a06d0ea59f824de
WillForan <willforan@gmail.com>
2022-09-01 11:01:05 -0400

    🐛 dollarreward/MR: PsychoPy3 and local dll

--- desktop_run_dollarreward_noTR.bat
+++ desktop_run_dollarreward_noTR.bat
@@ -1,6 +1,6 @@
-SET "VPXDLL=C:/Windows/System32/VPX_InterApp_64.dll"
+SET "VPXDLL=C:/Users/Luna/Desktop/lncdtask/dll/VPX_InterApp_64.dll
 SET "ET_HOST=10.48.88.120"
-SET "PYPATH=C:\Program Files\PsychoPy\python.exe"
+SET "PYPATH=C:\Program Files\PsychoPy3\python.exe"
 SET "TASKDIR=C:\Users\Luna\Desktop\lncdtask"
 cd %TASKDIR%
 "%PYPATH%" "%TASKDIR%\lncdtask\dollarreward.py" --where mr  --nruns 2  "timing/dollarreward/dollar_reward_noTR_304.2_10871.tsv"  "timing/dollarreward/dollar_reward_noTR_304.2_4341.tsv" "timing/dollarreward/dollar_reward_noTR_304.2_23695.tsv"  "timing/dollarreward/dollar_reward_noTR_304.2_5183.tsv" "timing/dollarreward/dollar_reward_noTR_304.2_25418.tsv"  "timing/dollarreward/dollar_reward_noTR_304.2_7243.tsv"
