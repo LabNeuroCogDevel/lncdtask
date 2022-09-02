c48b821c80c2d953edf5f43fcd7b5b27dde2f293
WillForan <willforan@gmail.com>
2022-09-02 16:42:37 -0400

    ✨ eyecal: run eye calibration

---
+++ lncdtask/eyecal.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+try:
+    from lncdtask import LNCDTask, create_window, replace_img, wait_for_scanner
+except ImportError:
+    from lncdtask.lncdtask import LNCDTask, create_window, replace_img, wait_for_scanner
+import numpy as np
+import pandas as pd
+
+
+def random_positions(center=.2, edge=.9, n=20):
+    # default is 20 steps from center .2 to edge .9 (right side)
+    # left side is that but negative: center -.2 to edge -.9
+    # for the right side
+    p_r = np.linspace(center, edge, n)
+    p_lr = np.concatenate([p_r, -1 * p_r])
+    ridx = np.random.permutation(len(p_lr))
+    return p_lr[ridx]
+
+
+def random_pos_df(dur=.5, **kargs):
+    positions = random_positions(**kargs)
+    events = []
+    onset = 0
+    for p in positions:
+        events.append({'event_name': 'iti',
+                       'position': p, 'onset': onset})
+        onset = onset + dur
+
+        events.append({'event_name': 'dot',
+                       'position': p, 'onset': onset})
+        onset = onset + dur
+
+    return pd.DataFrame(events)
+
+
+class EyeCal(LNCDTask):
+    """
+    show a dot to VGS to at a bunch of intervals
+    """
+    def __init__(self, *karg, **kargs):
+        """ create DollarReward task. like LNCDTask, need onset_df and maybe win and/or participant
+        this also adds a ringimg dictionary and an imageratsize (dotsize)
+        >> win = create_window(False)
+        >> dr = EyeCal(win=win, externals=[printer])
+        >> dr.dot(0, .75)
+        """
+        super().__init__(*karg, **kargs)
+
+        # extra stims/objects, exending base LNCDTask class
+        self.trialnum = 0
+
+        # events
+        self.add_event_type('dot', self.dot, ['onset', 'position'])
+        self.add_event_type('iti', self.iti, ['onset'])
+
+    def dot(self, onset, position=0):
+        """position dot on horz axis to cue anti saccade
+        position is from -1 to 1
+        """
+        self.trialnum = self.trialnum + 1
+        self.crcl.pos = (position * self.win.size[0]/2, 0)
+        self.crcl.size = (1, 1)
+        self.crcl.draw()
+        return(self.flip_at(onset, self.trialnum, 'dot', position))
+
+
+if __name__ == "__main__":
+    import sys
+    import argparse
+    from lncdtask import ExternalCom, RunDialog, FileLogger
+    printer = ExternalCom()
+    eyetracker = None
+
+    parser = argparse.ArgumentParser(description='Run Eye Calibration')
+    parser.add_argument('--tracker',
+                        choices=["arrington", "lpt", "testing"],
+                        default=None,
+                        help='how to track eyes')
+    parser.add_argument('--n_right',
+                        type=int,
+                        default=20,
+                        help='how many points to use')
+    parser.add_argument('--dur',
+                        type=float,
+                        default=0.5,
+                        help='how long to show each point')
+    parsed = parser.parse_args()
+    print(parsed)
+
+    run_info = RunDialog(extra_dict={'fullscreen': True,
+                                     'dur': parsed.dur,
+                                     'n_right': parsed.n_right},
+                         order=['subjid', 'run_num',
+                                'timepoint', 'fullscreen', 'dur', 'n_right'])
+
+    if not run_info.dlg_ok():
+        sys.exit()
+
+    dur = float(run_info.info['dur'])
+    n_right = int(run_info.info['n_right'])
+
+    # create task
+    win = create_window(run_info.info['fullscreen'])
+    eyecal = EyeCal(win=win, externals=[printer],
+                    onset_df=random_pos_df(dur=dur, n=n_right))
+    eyecal.gobal_quit_key()  # escape quits
+    eyecal.DEBUG = False
+
+    participant = run_info.mk_participant(['EyeCal'])
+    run_id = f"{participant.ses_id()}_task-EC_run-{run_info.run_num()}"
+
+    if parsed.tracker == 'arrington':
+        from externalcom import Arrington
+        eyetracker = Arrington()
+        eyecal.externals.append(eyetracker)
+        eyetracker.new(run_id)
+
+    logger = FileLogger()
+    logger.new(participant.log_path(run_id))
+    eyecal.externals.append(logger)
+
+    eyecal.run()
+    eyecal.onset_df.to_csv(participant.run_path(f"run-{run_info.run_num()}_info"))
+    win.close()
