a061ca08b405d4e1189c15be51311df5e4e2880d
WillForan <willforan@gmail.com>
2024-12-25 17:32:27 -0500

    🎨 rest.py: blacken, add docs

--- lncdtask/rest.py
+++ lncdtask/rest.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
+"""
+Rest "task" can be used
+ 1. Without participant facing visuals (task projector off) to track MR Scanner TR TTL (scanner sent '=')
+    a. Forwarded TR events to an eye track for syncing MR timing
+    b. Measuring TR intervals for sequence and hardware debugging. Background changes color based on start and unequal time between TR.
+ 2. With visual presented to the participant (w/o without a fixation cross)
+"""
 try:
-    from lncdtask import LNCDTask, create_window, replace_img, \
-            wait_for_scanner, ExternalCom, RunDialog, FileLogger
+    from lncdtask import (
+        LNCDTask,
+        create_window,
+        replace_img,
+        wait_for_scanner,
+        ExternalCom,
+        RunDialog,
+        FileLogger,
+    )
 except ImportError:
-    from lncdtask.lncdtask import LNCDTask, create_window, replace_img,\
-            wait_for_scanner, ExternalCom, RunDialog, FileLogger
+    from lncdtask.lncdtask import (
+        LNCDTask,
+        create_window,
+        replace_img,
+        wait_for_scanner,
+        ExternalCom,
+        RunDialog,
+        FileLogger,
+    )
 
 import sys
 import numpy as np
@@ -15,10 +36,11 @@ from psychopy import core, event
 
 class RestTask(LNCDTask):
     """
-    show an empty screen. send scanner tll ('=') onto EyeLink
+    Show an empty screen. Send scanner TTL ('=') onto EyeLink
     """
+
     def __init__(self, *karg, **kargs):
-        """ create DollarReward task. like LNCDTask,
+        """create DollarReward task. like LNCDTask,
         need onset_df and maybe win and/or participant
         this also adds a ringimg dictionary and an imageratsize (dotsize)
         >> win = create_window(False)
@@ -28,31 +50,36 @@ class RestTask(LNCDTask):
         """
         super().__init__(*karg, **kargs)
 
-
     def with_instructions(self, cnt=None, time=None, code="rest flip"):
-        """show instructions about resting state
-        """
+        """show instructions about resting state"""
         self.mark_external(code)
-        self.msgbox.pos = (0,.6)
-        self.msgbox.text = 'Turn of projector.\nInstructions: keep eyes open and let mind wonder.\n\nPush q to quit'
+        self.msgbox.pos = (0, 0.6)
+        self.msgbox.text = (
+            "Turn of projector.\n"
+            + "Instructions: keep eyes open and let mind wonder.\n\n"
+            + "Push q to quit"
+        )
         self.msgbox.draw()
         if cnt or time:
-            self.msgbox.pos = (0,0)
-            self.msgbox.text = f'Scanner TTL/TR pulse count: {cnt}\nTotal rest time: {time}'
+            self.msgbox.pos = (0, 0)
+            self.msgbox.text = (
+                f"Scanner TTL/TR pulse count: {cnt or 0}\n"
+                + f"Total rest time: {time or 0}"
+            )
             self.msgbox.draw()
         flip = self.win.flip()
-        return({'flip': flip})
+        return {"flip": flip}
 
     def run(self, cross):
         if cross:
             flip_on = self.iti(code="rest launched")
         else:
-            self.with_instructions(0,0)
+            self.with_instructions(0, 0)
 
         starttime = 0
         cnt = 0
         while True:
-            key = event.waitKeys(keyList=["equal","q"])
+            key = event.waitKeys(keyList=["equal", "q"])
             print(key)
             pushtime = core.getTime()
             if key[0] == "q":
@@ -64,57 +91,63 @@ class RestTask(LNCDTask):
             msg = f"{cnt} {time}"
             cnt = cnt + 1
             if cross:
-                self.iti(0,code=msg)
+                self.iti(0, code=msg)
             else:
                 self.with_instructions(cnt, time, code=msg)
 
 
 def parse_args(argv):
     import argparse
-    parser = argparse.ArgumentParser(description='Run Eye Calibration')
-    parser.add_argument('--tracker',
-                        choices=["eyelink"],
-                        default="eyelink",
-                        help='how to track eyes')
-    parser.add_argument('--no_fullscreen',
-                        default=False,
-                        action='store_true',
-                        help='show task fullscreen? useful for debugging')
-    parser.add_argument('--cross',
-                        default=False,
-                        action='store_true',
-                        help='show fixation cross; otherwise expect screen to be turned off')
-    parser.add_argument('--subj_root',
-                        default=Path.home()/"Desktop"/"task_data",
-                        help='where')
+
+    parser = argparse.ArgumentParser(description="Run Eye Calibration")
+    parser.add_argument(
+        "--tracker", choices=["eyelink"], default="eyelink", help="how to track eyes"
+    )
+    parser.add_argument(
+        "--no_fullscreen",
+        default=False,
+        action="store_true",
+        help="show task fullscreen? useful for debugging",
+    )
+    parser.add_argument(
+        "--cross",
+        default=False,
+        action="store_true",
+        help="show fixation cross; otherwise expect screen to be turned off",
+    )
+    parser.add_argument(
+        "--subj_root", default=Path.home() / "Desktop" / "task_data", help="where"
+    )
 
     parsed = parser.parse_args(argv)
-    return(parsed)
+    return parsed
 
 
 def run_rest(parsed):
     printer = ExternalCom()
     eyetracker = None
-    run_info = RunDialog(extra_dict={'show_cross': parsed.cross,
-                                     'fullscreen': not parsed.no_fullscreen,
-                                     'tracker': parsed.tracker,
-                                     },
-                         order=['subjid', 'run_num',
-                                'timepoint', 'show_cross', 'tracker'])
+    run_info = RunDialog(
+        extra_dict={
+            "show_cross": parsed.cross,
+            "fullscreen": not parsed.no_fullscreen,
+            "tracker": parsed.tracker,
+        },
+        order=["subjid", "run_num", "timepoint", "show_cross", "tracker"],
+    )
 
     if not run_info.dlg_ok():
         sys.exit()
 
     # create task
-    win = create_window(run_info.info['fullscreen'])
+    win = create_window(run_info.info["fullscreen"])
     eyecal = RestTask(win=win, externals=[printer])
     eyecal.gobal_quit_key()  # escape quits
     eyecal.DEBUG = True
 
-    participant = run_info.mk_participant(['RestTask'], subj_root=parsed.subj_root)
+    participant = run_info.mk_participant(["RestTask"], subj_root=parsed.subj_root)
     run_id = f"{participant.ses_id()}_task-rest_run-{run_info.run_num()}"
 
-    if run_info.info['tracker'] == 'arrington':
+    if run_info.info["tracker"] == "arrington":
         try:
             # as module
             from lncdtask.externalcom import Arrington
@@ -124,7 +157,7 @@ def run_rest(parsed):
         eyetracker = Arrington()
         eyecal.externals.append(eyetracker)
         eyetracker.new(run_id)
-    elif run_info.info['tracker'] == "eyelink":
+    elif run_info.info["tracker"] == "eyelink":
         try:
             # as module
             from lncdtask.externalcom import Eyelink
@@ -139,7 +172,7 @@ def run_rest(parsed):
     logger.new(participant.log_path(run_id))
     eyecal.externals.append(logger)
 
-    eyecal.run(run_info.info['show_cross'])
+    eyecal.run(run_info.info["show_cross"])
     win.close()
 
 
